6939a888c7a2f9a0a879b7682685d57cd9c15f3a
Mai Hoang Hai <hoanghai27@gmail.com>
2016-06-11 22:45:38 +0700

    add descriptions

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # instagram-tricks
 Some tricks to interact with Instagram that are not in official API
+
+# Requirements
+- Python 2.7
+- [Requests library](http://docs.python-requests.org/en/master/user/install/)
+
+# How to run
+- Change password:
+```
+python change_password.py username password new_password
+```
+
+- Upload photo:
+```
+python post_photo.py username password path_to_your_square image
+```
--- post_photo.py
+++ post_photo.py
@@ -1,16 +1,19 @@
 from instagram import InstagramSession
 import sys
 
+# Get username, password and image path from arguments
 username = sys.argv[1]
 password = sys.argv[2]
 file_path = sys.argv[3]
-print "Uploading " + file_path
 
+# NOTE: Image must be square
+# Library is ported from PHP at http://lancenewman.me/posting-a-photo-to-instagram-without-a-phone/
+# Thanks @lukecyca
+
+print "Uploading " + file_path
 insta = InstagramSession()
 if insta.login(username, password):
     media_id = insta.upload_photo(file_path)
     print media_id
     if media_id is not None:
         insta.configure_photo(media_id, "")
-    else:
-        raise Exception("Photo uploaded fail")
